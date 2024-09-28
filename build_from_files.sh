cb7883aadd1db1d3fa5aa36eb6db26c4135c6f2b
IJCAI2024AnonymousSubmission <suzhixiang1999@live.cn>
2024-09-28 18:46:48 +0800

    bug fix

--- build_from_files.sh
+++ build_from_files.sh
@@ -2,7 +2,7 @@ python build_graph/main.py --url bolt://localhost \
                             --username [YOUR_NEO4J_USERNAME] \
                             --password [YOUR_NEO4J_PASSWORD] \
                             --port 7687 \
-                            --load_path movies \
+                            --load_path disease \
                             --openai_api_key [YOUR_OPENAI_API_KEY] \
                             --openai_model_name gpt-3.5-turbo \
                             --temperature 0 \
